25826eed3a46c4cc29856387708c73bcf7a2bbe5
betensis <betensis@gmail.com>
2024-07-06 17:31:16 +0200

    хуяк хуяк и аналитика!

--- www/src/Controller/AiController.php
+++ www/src/Controller/AiController.php
@@ -139,7 +139,95 @@ final class AiController extends BaseController
 				'post' => $post,
 			],
 		);
+	}
 
+	#[Route(path: "/api/v1/analytic/generate/image", name: "app_ai_analytic", methods: ["POST"])]
+	#[OA\RequestBody(
+		description: "Create a new group",
+		required: true,
+		content: new OA\JsonContent(
+			properties: [
+							new OA\Property(property: "postId", type: "string"),
+							new OA\Property(property: "groupId", type: "string"),
+						],
+			type:       "object",
+		)
+	)]
+	#[OA\Response(
+		response: 200,
+		description: "Returns the rewards of a user",
+		content: new OA\JsonContent(
+			properties: [
+							new OA\Property(property: "status", type: "string", description: "Response status"),
+							new  OA\Property(property: "data", type: "object", description: "Request data"),
+						],
+			type:       "object",
+		)
+	)]
+	public function sendImage(
+		Request $request,
+		VkApiConnector $vkApiConnector,
+		RabbitMQProducerService $producerService,
+	): Response
+	{
+		$jsonka = json_decode($request->getContent(), true);
+		$postId = (string)($jsonka['postId'] ?? 0);
+		if (!$postId)
+		{
+			return $this->jsonResponseWithError('has no post by id');
+		}
+		$groupId = (string)($jsonka['groupId'] ?? 0);
+		if (!$groupId)
+		{
+			return $this->jsonResponseWithError('has no group by id');
+		}
+		$comments = $vkApiConnector->getComments($groupId, $postId);
+		$post = $vkApiConnector->getPost($groupId, $postId);
+		$commentsMe = [];
+
+		foreach ($comments['items'] as $comment)
+		{
+			$user_id = $comment['from_id'];
+			$user_profile = array_values(
+								array_filter(
+									$comments['profiles'],
+									function($profile) use ($user_id) {
+										return $profile['id'] == $user_id;
+									},
+								),
+							)[0];
+
+			$commentsMe[] = [
+				'username' => $user_profile['first_name'] . ' ' . $user_profile['last_name'],
+				'text' => $comment['text'],
+				'likes' => $comment['likes']['count'] ?? 0,
+				'attachments' => [],
+			];
+		}
+
+		$post = $post['items'][0];
+		$message = [
+			'type' => "IMAGE",
+			'id' => $postId . '.' . $groupId . '.' . GenerateResult::IMAGE,
+			'content' => [
+				'post' => [
+					'text' => $post['text'],
+					'likes' => $post['likes']['count'] ?? 0,
+					'attachments' => [],
+				],
+				'comments' => $commentsMe,
+			],
+		];
+
+		$producerService->sendMessage(new OpenAIGeneralMessage(json_encode($message)), 'analysis');
+
+		return $this->json(
+			[
+				'message' => $message,
+				'comments' => $comments,
+				'post' => $post,
+			],
+		);
 	}
 
 	#[Route(path: "/api/v1/analytic/{postId}/{groupId}", name: "app_ai_analytic_by_post", methods: ["get"])]
