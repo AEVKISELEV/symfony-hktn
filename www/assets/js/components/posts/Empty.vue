bdacd0b5b3ba88e2248744a0402456ee89fcb2c4
ArtiResh <art.reshetov39@gmail.com>
2024-07-06 16:06:51 +0200

    Компоненты списка постов

---
+++ www/assets/js/components/posts/Empty.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="post--empty-list" class="post-empty-list-block">
+		Здесь будут посты
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Empty',
+	data() {
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
