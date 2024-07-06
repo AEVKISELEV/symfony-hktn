27505560e487737191f232297b0914623078363a
nessai1 <werowkin@gmail.com>
2024-07-06 10:13:51 +0200

    go: rabbitMQ config added

--- docker/go/src/internal/service/service.go
+++ docker/go/src/internal/service/service.go
@@ -4,20 +4,41 @@ import (
 	"fmt"
 	"github.com/AEVKISELEV/symfony-hktn/internal/logger"
 	"go.uber.org/zap"
+	"os"
 )
 
 type Service struct {
 	logger *zap.Logger
+	config Config
+}
+
+type Config struct {
+	RabbitMQAddress string
+}
+
+func readConfig() (Config, error) {
+	rabbit, ok := os.LookupEnv("RABBITMQ_ADDRESS")
+	if !ok {
+		return Config{}, fmt.Errorf("cannot read rabbitmq address from env")
+	}
+
+	return Config{
+		RabbitMQAddress: rabbit,
+	}, nil
 }
 
 func Start() error {
-
 	s := Service{}
 	var err error
 
 	s.logger, err = logger.BuildLogger()
 	if err != nil {
 		return fmt.Errorf("cannot build logger: %w", err)
+	}
+
+	s.config, err = readConfig()
+	if err != nil {
+		return fmt.Errorf("cannot read config: %w", err)
 	}
 
 	s.logger.Info("Service started")
