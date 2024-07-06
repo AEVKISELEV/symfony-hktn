5495c8a2c7d67d75f33d5cc5512ab1ec760e5ec3
betensis <betensis@gmail.com>
2024-07-06 09:47:22 +0200

    init

---
+++ www/src/Controller/FrontendController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FrontendController extends AbstractController
+{
+	#[Route("/", 'app_index', methods: ['GET'])]
+	public function index(): Response
+	{
+		if (!$this->getUser())
+		{
+			return $this->redirectToRoute('app_login');
+		}
+
+		return $this->render('index.html.twig');
+	}
+}
